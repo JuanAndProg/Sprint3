81a2f7f71d6adf6cd4a61ac94b222beafa4919a3
JuanAndProg <juanandresmunoz85@gmail.com>
2023-06-13 11:52:41 +0200

    creation of the 2nd exercise with the requirements of the statement

---
+++ Tema3/Nivell2 - Adapter/poultry.php
@@ -0,0 +1,27 @@
+<?php
+
+class Duck{
+
+       public function quack(){
+              echo "Quack \n";
+       }
+
+       public function fly(){
+              echo "I'm flying \n";
+       }
+}
+
+class Turkey{
+
+       public function gobble(){
+              echo "Gobble gobble \n";
+       }
+
+       public function fly(){
+              echo "I'm flying a short distance \n";
+       }
+}
+class TurkeyAdapter implements Duck{
+       private $turkey;
+}
+?>
