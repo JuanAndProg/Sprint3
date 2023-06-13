87f33b3487ac6387c3f49b9ab5f306cf709ede7a
JuanAndProg <juanandresmunoz85@gmail.com>
2023-06-13 12:39:02 +0200

    structure of the adapter and defining functions

--- Tema3/Nivell2 - Adapter/index.php
+++ Tema3/Nivell2 - Adapter/index.php
@@ -1,18 +1,22 @@
 <?php
-function duck_interaction($duck)
-{
-    $duck->quack();
-    $duck->fly();
-}
+require_once 'poultry.php';
 
 $duck = new Duck;
 $turkey = new Turkey;
 $turkey_adapter = new TurkeyAdapter($turkey);
+
+function duck_interaction($duck){
+    $duck->quack();
+    $duck->fly();
+}
+
 echo "The Turkey says...\n";
 $turkey->gobble();
 $turkey->fly();
+
 echo "The Duck says...\n";
 duck_interaction($duck);
+
 echo "The TurkeyAdapter says...\n";
 duck_interaction($turkey_adapter);
 ?>
--- Tema3/Nivell2 - Adapter/poultry.php
+++ Tema3/Nivell2 - Adapter/poultry.php
@@ -20,8 +20,26 @@ class Turkey{
        public function fly(){
               echo "I'm flying a short distance \n";
        }
-}
-class TurkeyAdapter implements Duck{
+}class TurkeyAdapter implements Duck {
        private $turkey;
-}
+   
+       public function __construct(Turkey $turkey) {
+           $this->turkey = $turkey;
+       }
+   
+       public function quack() {
+           $this->turkey->gobble();
+       }
+   
+       public function fly() {
+           for ($i = 0; $i < 5; $i++) {
+               $this->turkey->fly();
+           }
+       }
+   }
+   
+   function duck_interaction($duck) {
+       $duck->quack();
+       $duck->fly();
+   }
 ?>
