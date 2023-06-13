7ac27a04507e73d0f4d14500bd4f9f332a318d24
JuanAndProg <juanandresmunoz85@gmail.com>
2023-06-13 09:51:46 +0200

    first commit, statement

---
+++ Tigger.php
@@ -0,0 +1,13 @@
+<?php
+class Tigger {
+
+       private function __construct() {
+               echo "Building character..." . PHP_EOL;
+       }
+
+       public function roar() {
+               echo "Grrr!" . PHP_EOL;
+       }
+
+}
+?>
